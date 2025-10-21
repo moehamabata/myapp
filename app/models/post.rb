68a25b8cf83c8f0bf60033b549f71eec39aabe2f
moe <hamabatamoe@gmail.com>
2025-10-21 22:43:00 +0900

    追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,3 @@
-require 'rails_helper'
-
-RSpec.describe Post, type: :model do
-  it 'タイトルがあれば有効であること' do
-    post=Post.new(title: 'テストタイトル',content: '本文')
-    expect(post).to be_valid
-  end
-
-  it 'タイトルがなければ無効であること' do
-    post=Post.new(title: nil,content: '本文')
-    expect(post).not_to be_valid
-  end
+class Post < ApplicationRecord
+  validates :title, presence: true
 end
