7f5bf2189ef6f3f46110db20640a768960de7d97
moe <hamabatamoe@gmail.com>
2025-10-23 19:36:54 +0900

    文字上限数を設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 50}
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'タイトルがあれば有効であること' do
+  it 'タイトルが3文字以上であれば有効であること' do
     post = Post.new(title: 'テストタイトル', content: '本文')
     expect(post).to be_valid
   end
 
-  it 'タイトルがなければ無効であること' do
-    post = Post.new(title: nil, content: '本文')
+  it 'タイトルが3文字未満であれば無効であること' do
+    post = Post.new(title: '短い', content: '本文')
     expect(post).not_to be_valid
   end
 end
