faf3cf760fa69613c12074ffd37ba7a7d72811c8
moe <hamabatamoe@gmail.com>
2025-10-23 21:42:28 +0900

    pending修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
+  validates :content, presence: true, length: { maximum: 450 }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Post, type: :model do
     post = Post.new(title: '短い', content: '本文')
     expect(post).not_to be_valid
   end
+
+  it '同じタイトルが存在する場合は無効であること' do
+    Post.create(title: 'タイトル', content: '本文') #まず既存の投稿を作る
+    duplicate_post = Post.new(title: 'タイトル', content: '別の本文')
+    expect(duplicate_post).not_to be_valid
+  end
+
+  it '本文が451文字以上の場合は無効であること' do
+    long_content = 'あ' * 451
+    post = Post.new(title: 'タイトル', content: long_content)
+    expect(post).not_to be_valid
+  end
 end
